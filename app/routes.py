4a7988f6d47567c6094a8bf93166da5c5f7634f4
finc8888 <con8@mail.ru>
2018-04-16 20:51:04 +0300

    'extends'

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,31 @@
 from  app import app
+from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Привет мир!"
+	users = {'username': 'владимир'}
+	posts = [
+	{
+	'author':{'username':'Fedja'},
+	'body':'Все путем!'
+	},
+	{
+	'author': {'username':'Lavnikevich'},
+	'body': 'Копипастинг'
+	},
+	{
+	'author': {'username':'Andruha'},
+	'body': 'Автоматизация в массы!'
+	}
+	]
+	return render_template('index.html', \
+		title= 'My',users = users, posts = posts)
+'''
+<html>
+	<head>
+		<title>My microblog</title>
+	</head>
+	<body>
+		<h1>Hello,+ users['user'].title() +!</h1>
+	</body>
+</html>'''
