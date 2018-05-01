4584b72d609cc150789a9093ea8717dec451feb2
Finc8888 <con8@mail.ru>
2018-05-01 23:36:02 +0300

    SQLAlchemy

--- config.py
+++ config.py
@@ -1,8 +1,19 @@
 import os
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     """Параметры конфигурации преложения."""
 
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you - will - never - guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' +\
+        os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    """
+    Расширение Flask-SQLAlchemy принимает местоположение базы данных приложения
+    из переменной конфигурации SQLALCHEMY_DATABASE_URI.
+    """
+    """
+    Параметр конфигурации SQLALCHEMY_TRACK_MODIFICATIONS установлен в значение
+    False, чтобы отключить функцию Flask-SQLAlchemy, которая  не нужна
+    """
--- microblog.py
+++ microblog.py
@@ -1,1 +1,16 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+"""
+Декоратор app.shell_context_processor регистрирует функцию как функцию
+контекста оболочки. Когда запускается команда flask shell, она будет вызывать
+эту функцию и регистрировать элементы, возвращаемые ею в сеансе оболочки.
+"""
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
+"""
+После того, как вы добавите функцию обработчика flask shell, вы можете работать
+с объектами базы данных, не импортируя их
+"""
